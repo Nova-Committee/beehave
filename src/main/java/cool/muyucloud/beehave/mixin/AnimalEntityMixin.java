e5b7b13e77900c2b7de20d3162a5e5a08b576bd4
MUYU_Twilighter <1484605372@qq.com>
2024-08-09 21:55:44 -0400

    Port to 21 Add particles showing directions for a bee's hive Improvements in texts color of beehive info. Add command and config

--- src/main/java/cool/muyucloud/beehave/mixin/AnimalEntityMixin.java
+++ src/main/java/cool/muyucloud/beehave/mixin/AnimalEntityMixin.java
@@ -1,6 +1,7 @@
 package cool.muyucloud.beehave.mixin;
 
 import cool.muyucloud.beehave.Beehave;
+import cool.muyucloud.beehave.access.BeeEntityAccess;
 import cool.muyucloud.beehave.config.Config;
 import cool.muyucloud.beehave.util.TranslatorManager;
 import net.minecraft.entity.EntityType;
@@ -8,12 +9,17 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -44,6 +50,25 @@ public abstract class AnimalEntityMixin extends PassiveEntity {
         BeeEntity entity = ((BeeEntity) (Object) this);
         MutableText beeInfo = getBeeInfo(entity);
         player.sendMessage(beeInfo, false);
+        playParticles(entity.getHivePos(), (ServerPlayerEntity) player);
+    }
+
+
+    private void playParticles(@NotNull BlockPos hivePos, @NotNull ServerPlayerEntity target) {
+        final int density = 3;
+        Vec3d beePos = this.getPos();
+        if (!hiveAvailable((BeeEntity) (Object) this)) {
+            return;
+        }
+        Vec3d delta = beePos.relativize(new Vec3d(hivePos.getX() + 0.5D, hivePos.getY() + 0.5D, hivePos.getZ() + 0.5D));
+        double distance = delta.length();
+        Vec3d step = delta.multiply(1.0D / (density * distance), 1.0D / (density * distance), 1.0D / (density * distance));
+        int count = (int) (distance * density);
+        Vec3d pos = beePos;
+        for (int i = 0; i <= count; ++i) {
+            ((ServerWorld) this.getWorld()).spawnParticles(target, ParticleTypes.HAPPY_VILLAGER, false, pos.getX(), pos.getY(), pos.getZ(), 1, 0.0D, 0.0D, 0.0D, 0.0D);
+            pos = pos.add(step);
+        }
     }
 
     private static MutableText getBeeInfo(BeeEntity entity) {
@@ -51,7 +76,7 @@ public abstract class AnimalEntityMixin extends PassiveEntity {
         if (entity.getHivePos() != null && hiveAvailable(entity)) {
             BlockPos pos = entity.getHivePos();
             text.append(TRANSLATOR.translate(
-                "message.chat.bee.info", pos.getX(), pos.getY(), pos.getZ()));
+                    "message.chat.bee.info", pos.getX(), pos.getY(), pos.getZ()));
         } else {
             text.append(TRANSLATOR.translate("message.chat.bee.homeless"));
         }
@@ -60,6 +85,6 @@ public abstract class AnimalEntityMixin extends PassiveEntity {
 
     private static boolean hiveAvailable(BeeEntity entity) {
         return entity.hasHive() &&
-            ((BeeEntityAccessor) entity).invokeDoesHiveHaveSpace(entity.getHivePos());
+                ((BeeEntityAccess) entity).invokeDoesHiveHaveSpace(entity.getHivePos());
     }
 }
